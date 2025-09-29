021e3ce0aaf5cfcc1d76c6e02d9b546643a183ff
Naveen-kumar32 <nilanaveen32@gmail.com>
2025-09-29 17:57:50 +0530

    Initial commit - pushing existing code

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
 import Layout from "@/components/layout/Layout"
-// import Home2 from "@/components/pages/home2"
-import Home1 from "@/components/pages/home1"
+import Home2 from "@/components/pages/home2"
 export default function Home() {
 
 	return (
 		<>
 			<Layout headerStyle={2} footerStyle={2} hero={2} mainCls="style-2">
-				{/* <Home2 /> */}
-				<Home1 />
+				<Home2 />
 			</Layout>
 		</>
 	)
